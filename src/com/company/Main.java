9a02f5e9bfb2c75bfc17f4917f4f6b67dacd4b5d
Shubham Mishra <shubham.mishra03@sap.com>
2022-03-23 17:02:52 +0530

    send output to outputPS4.txt

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -39,8 +39,8 @@ public class Main {
     public static void main(String[] args) {
         BufferedReader reader;
         try {
-            reader = new BufferedReader(new FileReader(
-                    "C://Users//hp//OneDrive//Documents//GitHub//dsa//lines.txt"));
+            reader = new BufferedReader(new FileReader("lines.txt"));
+//          reader=  new BufferedReader(new FileReader("C://Users//hp//OneDrive//Documents//GitHub//dsa//lines.txt"));
             String line = reader.readLine();
             while (line != null) {
 //                System.out.println(line);
@@ -54,6 +54,30 @@ public class Main {
                 System.out.println();
             }
             reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        /*for printing to output file outputPS4.txt*/
+        printToOutputFile(samples);
+    }
+    public static void printToOutputFile(List<Sample> samples){
+        BufferedWriter writer;
+        try {
+            writer = new BufferedWriter(new FileWriter(
+                    "outputPS4.txt"));
+            int i=1;
+            for(Sample sample:samples) {
+                writer.write("Top Books " + i++ + ": " + sample.getObj1() + ", " + sample.getObj2()+"\n");
+            }
+            writer.write("\nList of books not issued:\n");
+            for(Sample sample:samples){
+                writer.write("" + sample.getObj1()+"\n");
+            }
+            writer.write("\nBook id "+samples.get(0).getObj1()+" is available for checkout.\n");
+            writer.write("\nAll available copies of the below books have been checked out:\n");
+            writer.write(""+samples.get(1).getObj1());
+            writer.flush();
+            writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
