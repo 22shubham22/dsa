e6ce0367f7a90ef11f5054835fbc2e5ea263561f
Shubham Anand <60786471+22shubham22@users.noreply.github.com>
2022-03-25 10:09:47 +0530

    intro to tree

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -37,6 +37,80 @@ class attributeDetails{
         this.bkId = bkId;
     }
 }
+class BookNode {
+    public int bookId;
+    public int availableCount;
+    public int checkoutCounter;
+    BookNode leftBook;
+    BookNode rightBook;
+
+    BookNode() { // empty constructor to set integers to 0 and reference nodes to null
+        this.bookId=0;
+        this.availableCount=0;
+        this.checkoutCounter=0;
+        this.leftBook=null;
+        this.rightBook=null;
+    }
+
+    BookNode(int bookId,int availableCount, int checkoutCounter) { // parametrized constructor to set integers with parameters and reference nodes to null
+        this.bookId=bookId;
+        this.availableCount=availableCount;
+        this.checkoutCounter=checkoutCounter;
+        this.rightBook=null;
+        this.leftBook=null;
+    }
+}
+
+class LibraryManagement {
+    BookNode rootBook;
+    LibraryManagement() {
+        rootBook= null;
+    }
+
+    public void insertBook(BookNode book) {
+        if (this.rootBook==null) { // if root node is null, the root node is assigned the book
+             this.rootBook=book;
+        }
+        else {
+            BookNode currentBook=this.rootBook;
+            BookNode previousBook;
+            while (true) { // will be terminated internally
+                previousBook=currentBook; // to keep track of previous book(node) , in case current book(node) is null
+                if(book.bookId < currentBook.bookId ) { // then go left
+                    currentBook=currentBook.leftBook;
+                    if (currentBook == null) { //if left book (node) is null i.e empty
+                        previousBook.leftBook=book; // then assign this place to current book
+                        return;
+                    }
+                }
+                else { // else , go right
+                    currentBook=currentBook.rightBook;
+                    if (currentBook == null) { //if right book (node) is null i.e empty
+                        previousBook.rightBook=book; // then assign this place to current book
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    public BookNode findBook(int bookId) {
+        BookNode currentBook= this.rootBook; // assign root node as current node to begin traversal for search
+        while (currentBook.bookId != bookId) { // till we dont find book with given Book ID
+            if (bookId < currentBook.bookId) { // if less the go to left book
+                currentBook=currentBook.leftBook;
+            }
+            else {
+                currentBook=currentBook.rightBook; // if less the go to right book
+            }
+            if(currentBook == null) { // if we reach end but haven't found return null book
+                return null;
+            }
+        }
+        return currentBook; // return the book that we found while traversal
+    }
+}
+
 class Sample {
     int obj1;
     int obj2;
@@ -68,13 +142,19 @@ public class Main {
 
     static List<Sample> samples = new ArrayList<Sample>();
     public static void main(String[] args) {
+        //for reading from input1.txt
+        readFromFile();
+        //for printing to output file outputPS4.txt
+        printToOutputFile(samples);
+    }
+
+    public static void readFromFile() {
+
         BufferedReader reader;
         try {
             reader = new BufferedReader(new FileReader("lines.txt"));
-//          reader=  new BufferedReader(new FileReader("C://Users//hp//OneDrive//Documents//GitHub//dsa//lines.txt"));
             String line = reader.readLine();
             while (line != null) {
-//                System.out.println(line);
                 List<String> items = Arrays.asList(line.split("\\s*,\\s*"));
                 samples.add(new Sample(Integer.parseInt(items.get(0)),Integer.parseInt(items.get(1))));
                 // read next line
@@ -88,14 +168,13 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        /*for printing to output file outputPS4.txt*/
-        printToOutputFile(samples);
     }
+
     public static void printToOutputFile(List<Sample> samples){
+
         BufferedWriter writer;
         try {
-            writer = new BufferedWriter(new FileWriter(
-                    "outputPS4.txt"));
+            writer = new BufferedWriter(new FileWriter("outputPS4.txt"));
             int i=1;
             for(Sample sample:samples) {
                 writer.write("Top Books " + i++ + ": " + sample.getObj1() + ", " + sample.getObj2()+"\n");
@@ -117,11 +196,9 @@ public class Main {
     public void inOutCheckCounter() {
         ArrayList<attributeDetails> inOutDetails = new ArrayList<>();
         String line;
-
         try {
-            FileReader file = new FileReader("C:\\Users\\I526514\\Desktop\\DSA assignment\\promptsPS4.txt");
+            FileReader file = new FileReader("promptsPS4.txt");
             BufferedReader br = new BufferedReader(file);
-
             while((line = br.readLine())!=null){
                 String[] record = line.split(":");
 
@@ -130,7 +207,6 @@ public class Main {
 
                 inOutDetails.add(new attributeDetails(status,bkId));
             }
-
             for(int i=0; i<=inOutDetails.size(); i++)
             {
                 if(inOutDetails.get(i).getStatus().equals("checkOut"))
