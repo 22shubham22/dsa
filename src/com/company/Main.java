4b7ccab19d3f5dfc23adc86e40fe419ed35da999
I526683 <shubham.anand@sap.com>
2022-06-09 20:02:27 +0530

    edge cases handled and code refactoring
    empty file
    weapons 0
    negative or 0 for damage and weight

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,17 @@
-//package com.company; // to be commented when given to ma'am
+package com.company; // to be commented when given to ma'am
 
 
 import java.io.IOException;
 import java.io.*;
 import java.util.*;
+import java.util.Map.Entry;
 
 class TestCase {
     int weapons;
     int maxWeight;
     List<Integer> weightValues;
     List<Integer> damagevalues;
-    HashMap<Integer, Double> ratio;
+    Map<Integer, Double> ratio;
 
     TestCase(int weapons,int maxWeight,List<Integer> weightValues,List<Integer> damagevalues) {
         this.weapons = weapons;
@@ -22,7 +23,7 @@ class TestCase {
     public void calculateRatio()
     {
         //iterate weightvalues and damagevalues and add to ratio
-        HashMap<Integer, Double> localRatio = new HashMap<Integer, Double>();
+        HashMap<Integer, Double> localRatio = new HashMap<>();
         for(int i=0; i<weightValues.size(); i++)
         {
             double currRatio = ((double)damagevalues.get(i))/(weightValues.get(i));
@@ -38,26 +39,29 @@ class TestCase {
         System.out.println(maxWeight);
         System.out.println(weightValues);
         System.out.println(damagevalues);
-        //System.out.println(ratio);
+        System.out.println(ratio);
     }
-    
+    public String runValidationTest() {
+        String error = "";
+        if(weapons <= 0) {error = error.concat("Number of Weapons is Zero/Negative ");}
+        if(maxWeight <= 0) {error = error.concat("Maximum Weight is Zero/Negative ");}
+        for(int i=0;i<weightValues.size();i++){
+            if(weightValues.get(i) <= 0 || damagevalues.get(i) <= 0) {
+                error = error.concat("Weight/Damage of Ammunition is Zero/Negative. ");
+                break;
+            }
+        }
+        return error;
+    }
 
-    public HashMap<Integer, Double> sortByValue(HashMap<Integer, Double> hm)
+    public Map<Integer, Double> sortByValue(HashMap<Integer, Double> hm)
     {
         // Create a list from elements of HashMap
-        List<Map.Entry<Integer, Double> > list
-            = new LinkedList<Map.Entry<Integer, Double> >(
-                hm.entrySet());
- 
+        List<Map.Entry<Integer, Double> > list = new LinkedList<Map.Entry<Integer, Double> >(hm.entrySet());
         // Sort the list using lambda expression
-        Collections.sort(
-            list,
-            (i1,
-             i2) -> i1.getValue().compareTo(i2.getValue()));
- 
+        Collections.sort(list, Comparator.comparing(Entry::getValue,Comparator.reverseOrder()));
         // put data from sorted list to hashmap
-        HashMap<Integer, Double> temp
-            = new LinkedHashMap<Integer, Double>();
+        HashMap<Integer, Double> temp = new LinkedHashMap<Integer, Double>();
         for (Map.Entry<Integer, Double> aa : list) {
             temp.put(aa.getKey(), aa.getValue());
         }
@@ -68,24 +72,35 @@ public class Main {
     static BufferedWriter writer;
 
     public static void main(String[] args) {
+        Map<Integer,String> invalidTestcase;
         List<TestCase> testCases = new ArrayList<>();
-        _readBookList(testCases);  //for reading from inputPS4.txt
 
-        testCases.forEach(testcase ->{
-            testcase.calculateRatio();
-        });
+        readBookList(testCases);  //for reading from inputPS4.txt
 
-        testCases.forEach(testcase ->{
-            testcase.display();
-        });
+        testCases.forEach(TestCase::calculateRatio);
 
+        invalidTestcase = fetchInvalidTestCase(testCases);
+        System.out.println(invalidTestcase);
 
+        for(int i=0;i<testCases.size();i++) {
+            if(!invalidTestcase.containsKey(i)) {
+                testCases.get(i).display();
+            }
+        }
         List<Double> maxSize = new ArrayList<>();
         testCases.forEach(testCase -> {
             //for each iter, we obtain total value from the knapsack function and keep adding it to the size list
             double size = getMaxValue(testCase);
             maxSize.add(size);
         });
+        for(int i=0;i<testCases.size();i++) {
+            if(invalidTestcase.containsKey(i)) {
+                maxSize.add(-1.0);
+            }
+            else {
+                maxSize.add(getMaxValue(testCases.get(i)));
+            }
+        }
             
     }
 
@@ -109,12 +124,18 @@ public class Main {
     }
 
     //Operation 1
-    public static void _readBookList(List<TestCase> testCases) { 
+    public static void readBookList(List<TestCase> testCases) {
         BufferedReader reader;
         try {
             reader = new BufferedReader(new FileReader("inputPS4.txt")); 
-            String line = reader.readLine(); 
+            String line = reader.readLine();
+            if(line == null) {
+                System.out.println("Empty file"); //exception
+            }
             while (line != null) {
+                while(!line.toLowerCase().contains("weapons")) {
+                    line = reader.readLine();
+                }
                 String[] item1 = line.split(":"); 
                 line = reader.readLine(); 
                 String[] item2 = line.split(":"); 
@@ -123,7 +144,7 @@ public class Main {
                 String[] item;
                 List<Integer> weightValues = new ArrayList<>();
                 List<Integer> damageValues = new ArrayList<>();
-                for(int i=1; i<= Integer.parseInt(item1[1].trim()); i++) {
+                for(int i=0; i< Integer.parseInt(item1[1].trim()); i++) {
                     item = line.split("/");
                     weightValues.add(Integer.parseInt(item[1].trim()));
                     damageValues.add(Integer.parseInt(item[2].trim()));
@@ -135,6 +156,20 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace(); 
         }
+    }
+
+    private static Map<Integer,String> fetchInvalidTestCase(List<TestCase> t) { // for closing the writer object
+        HashMap<Integer,String> m = new HashMap<>();
+        for(int i=0;i<t.size();i++) {
+            String error = t.get(i).runValidationTest();
+            if(error.length() > 0) {
+                m.put(i,error);
+            }
+            else {
+                continue;
+            }
+        }
+        return m;
     }
 
     public static void triggerFunctionforPromptsPS4File() { 
@@ -167,10 +202,10 @@ public class Main {
  
         double totalValue = 0d;
  
-        for (Integer i : testcase.weightValues) {
+        for (int i=0; i<testcase.weightValues.size();i++) {
  
-            int curWt = (int)testcase.weightValues.get(i);
-            int curVal = (int)testcase.damagevalues.get(i);
+            int curWt = testcase.weightValues.get(i);
+            int curVal = testcase.damagevalues.get(i);
  
             if (capacity - curWt >= 0) {
                 // this weight can be picked while
@@ -179,11 +214,8 @@ public class Main {
             }
             else {
                 // item cant be picked whole
-                double fraction
-                    = ((double)capacity / (double)curWt);
-                totalValue += (curVal * fraction);
-                capacity
-                    = (int)(capacity - (curWt * fraction));
+                double fraction = ((double)capacity / (double)curWt);totalValue += (curVal * fraction);
+                capacity = (int)(capacity - (curWt * fraction));
                 break;
             }
         }
