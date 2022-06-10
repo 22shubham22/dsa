bdd4d8b8f9fd23e89c2cd8f604e3dd287cbda5c9
I526683 <shubham.anand@sap.com>
2022-06-10 15:52:32 +0530

    final tweaks

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -32,15 +32,6 @@ class TestCase { // Model of one particular test case from inputps4.txt
         ratio = sortByValue(localRatio);
     }
 
-    public void display() {
-        System.out.println();
-        System.out.println("<----------------------------------------------------------->");
-        System.out.println(weapons);
-        System.out.println(maxWeight);
-        System.out.println(weightValues);
-        System.out.println(damagevalues);
-        System.out.println(ratio);
-    }
     public String runValidationTest() {
         String error = "";
         if(weapons <= 0) {error = error.concat("Number of Weapons is Zero/Negative ");}
@@ -75,15 +66,6 @@ class Output { // Model of one particular output for corresponding testcase
         this.damage = damage;
         this.ratio = ratio;
     }
-    public void display() {
-        System.out.println();
-        System.out.println("<----------------------------------------------------------->");
-        System.out.println(damage);
-        for(int i=0; i<ratio.length;i++){
-            System.out.print(ratio[i]+" ");
-        }
-
-    }
 }
 public class Main {
     static BufferedWriter writer;
@@ -98,7 +80,6 @@ public class Main {
         testCases.forEach(TestCase::calculateRatio); // calculation of ratio of damage:weight and sort in reverse order
 
         invalidTestcase = fetchInvalidTestCase(testCases); // look for invalid test case with Negative/Zero values
-        System.out.println(invalidTestcase);
 
         for(int i=0;i<testCases.size();i++) { // call greedy algo only for valid test cases
             if(invalidTestcase.containsKey(i)) {
@@ -111,14 +92,13 @@ public class Main {
 
         initializeWriter();
         for(int i=0;i<outputs.size();i++) { // display outputs
-            if(invalidTestcase.containsKey(i)) {
+            if(invalidTestcase.containsKey(i)) { // appropriate message for invalid test case
                 System.out.println();
-                System.out.println("<----------------------------------------------------------->");
-                System.out.println(invalidTestcase.get(i)); // appropriate message for invalid testcase
+                System.out.println("Test Case Number "+i+" in the input file is invalid due to following reason(s):");
+                System.out.println(invalidTestcase.get(i));
             }
             else {
-                outputs.get(i).display();// valid outputs
-                writeToFile(outputs.get(i));
+                writeToFile(outputs.get(i)); // valid outputs
             }
         }
         closeWriter();
@@ -144,34 +124,40 @@ public class Main {
         }
     }
 
-    //Operation 1
-    public static void readInputFile(List<TestCase> testCases) {
+    public static void readInputFile(List<TestCase> testCases) { // read input file
         BufferedReader reader;
         try {
             reader = new BufferedReader(new FileReader("inputPS4.txt")); 
             String line = reader.readLine();
             if(line == null) {
-                System.out.println("Empty file"); //exception
+                System.out.println("Empty file"); //exception of empty file handled
             }
             while (line != null) {
-                while(!line.toLowerCase().contains("weapons")) {
+                while(!line.toLowerCase().contains("weapons")) { // to skip through invalid test case
                     line = reader.readLine();
                 }
-                String[] item1 = line.split(":"); 
+                String[] item1 = line.split(":"); // read weapons
                 line = reader.readLine(); 
-                String[] item2 = line.split(":"); 
-                line = reader.readLine(); 
-                
+                String[] item2 = line.split(":"); // read max weight
+                line = reader.readLine();
+                if(!isNumeric(item1[1].trim())) { item1[1] = "0"; } //exception of invalid number handled
+                if(!isNumeric(item2[1].trim())) { item2[1] = "0"; } //exception of invalid number handled
                 String[] item;
                 List<Integer> weightValues = new ArrayList<>();
                 List<Integer> damageValues = new ArrayList<>();
                 for(int i=0; i< Integer.parseInt(item1[1].trim()); i++) {
                     item = line.split("/");
-                    weightValues.add(Integer.parseInt(item[1].trim()));
-                    damageValues.add(Integer.parseInt(item[2].trim()));
+                    try{
+                        weightValues.add(Integer.parseInt(item[1].trim())); //exception of invalid number handled
+                        damageValues.add(Integer.parseInt(item[2].trim()));
+                    } catch (NumberFormatException e) {
+                        weightValues.add(-1);
+                        damageValues.add(-1);
+                    }
                     line = reader.readLine();
                 }
-                testCases.add(new TestCase(Integer.parseInt(item1[1].trim()),Integer.parseInt(item2[1].trim()),weightValues,damageValues));
+                testCases.add(new TestCase(Integer.parseInt(item1[1].trim()),Integer.parseInt(item2[1].trim()),
+                        weightValues,damageValues)); // create new testcase and add it to list
             }
             reader.close(); 
         } catch (IOException e) {
@@ -179,7 +165,16 @@ public class Main {
         }
     }
 
-    private static Map<Integer,String> fetchInvalidTestCase(List<TestCase> t) { // for closing the writer object
+    public static boolean isNumeric(String str) { // check for numeric character
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch(NumberFormatException e){
+            return false;
+        }
+    }
+
+    private static Map<Integer,String> fetchInvalidTestCase(List<TestCase> t) { // for testing validity of test case
         HashMap<Integer,String> m = new HashMap<>();
         for(int i=0;i<t.size();i++) {
             String error = t.get(i).runValidationTest();
@@ -193,37 +188,34 @@ public class Main {
         return m;
     }
 
-    // Fractional Knapsack Logic
-    private static void getMaxValue(TestCase testcase, List<Output> outputs)
+    private static void getMaxValue(TestCase testcase, List<Output> outputs) // Fractional Knapsack Logic
     {
         double[] ratio = new double[testcase.weightValues.size()];
         Arrays.fill(ratio,0.0);
         int capacity = testcase.maxWeight;
         double totalValue = 0d;
-        while (capacity > 0) {
-            for (int i: testcase.ratio.keySet()) {
+        while (capacity > 0) { // while we have capacity to take ammo
+            for (int i: testcase.ratio.keySet()) { //geting index from sorted ratio map
                 int curWt = testcase.weightValues.get(i);
                 int curVal = testcase.damagevalues.get(i);
-                if (capacity - curWt >= 0) {
-                    // this weight can be picked while
+                if (capacity - curWt >= 0) {// this weight can be picked whole
                     capacity = capacity - curWt;
                     totalValue += curVal;
                     ratio[i] = ratio[i] + 1.0;
                 }
-                else {
-                    // item can't be picked whole
+                else {// item can't be picked whole
                     double fraction = ((int)((double)capacity/curWt * 100))/100.0;
                     totalValue += (curVal * fraction);
                     capacity = (int)(capacity - (curWt * fraction));
                     ratio[i] = ratio[i] + fraction;
-                    break;
+                    break; // capacity over so break
                 }
             }
         }
-        outputs.add(new Output(totalValue,ratio));
+        outputs.add(new Output(totalValue,ratio)); // save output of one test case to a model to write to file later
     }
 
-    public static void writeToFile(Output op) throws IOException {
+    public static void writeToFile(Output op) throws IOException { // write to output file
         writer.write("Total Damage: ");
         writer.write(String.valueOf(op.damage));
         writer.newLine();
@@ -233,7 +225,7 @@ public class Main {
             writer.write("A");
             writer.write(String.valueOf(i+1));
             writer.write(" > ");
-            if(op.ratio[i] > (int)op.ratio[i])
+            if(op.ratio[i] > (int)op.ratio[i]) // check if 1.0 is same as 1
                 writer.write(String.valueOf(op.ratio[i]));
             else
                 writer.write(String.valueOf((int)op.ratio[i]));
